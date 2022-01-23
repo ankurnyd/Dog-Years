cbab09ca77541b3e3339bef449914b9ec676e028
Ankur Sharma <38402246+ankurnyd@users.noreply.github.com>
2022-01-24 03:12:31 +0530

    Done

---
+++ doGapp.js
@@ -0,0 +1,13 @@
+const myAge = 26;
+//Created a variable name MyAge and then I have set it to my age that is 27 years LOL!! 
+let earlyYears = 2;
+// Creating a varible name earlyears 
+earlyYears *= 10.5;
+laterYears = myAge - 2;
+//Since we already accounted for the first two years, take the myAge variable, and subtract 2 from it.
+laterYears *= 4; //multiplying the laterYears to 4.
+myAgeInDogYears = earlyYears + laterYears;
+let myName ='Ankur Sharma';
+myName.toLowerCase();  //The toLowerCase method returns a string with all lowercase letters.
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`);
+
